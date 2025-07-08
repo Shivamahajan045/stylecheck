53d763c21978b9a523fb37dc7ac6b15fb8b6ae1b
Shivakumar <111225580+Shivamahajan045@users.noreply.github.com>
2025-07-08 13:39:12 +0530

    updated code

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Product = require("./models/product");
 const Customer = require("./models/customer");
 const Invoice = require("./models/invoice");
 
+const valid_Schema = ["guru_foods", "fashion_king"];
+
 Customer.hasMany(Product);
 Product.belongsTo(Customer);
 
@@ -13,6 +15,31 @@ const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello</h1>`);
+});
+
+app.post("/invoices/add/:schema", async (req, res) => {
+  try {
+    const { schema } = req.params;
+    if (!valid_Schema.includes(schema)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Schema not valid" });
+    }
+
+    const { customerId, productId, quantity, total } = req.body;
+    const newInvoice = await Invoice.create({
+      customerId,
+      productId,
+      quantity,
+      total,
+    });
+
+    res
+      .status(201)
+      .json({ message: "New iNvoice created", Invoice: newInvoice });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong", err: err.message });
+  }
 });
 
 app.use(express.json());
